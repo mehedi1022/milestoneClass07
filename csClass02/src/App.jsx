0c9dff9ce4f6120cffddea33d817cc12489fb98a
mehedi1022 <mahdibinalamgir@gmail.com>
2025-01-04 04:19:44 +0600

    added SingleProduct in live server

--- csClass02/src/App.jsx
+++ csClass02/src/App.jsx
@@ -1,19 +1,17 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 
+import "./App.css";
+import SingleProduct from "./singleProduct";
 
 function App() {
   const [products, setProducts] = useState([]);
-
   useEffect(() => {
     fetch("./fakeData.json")
-    .then((res) => res.json())
-    .then((data) => {
-      setProducts(data);
-      console.log(data);
-    })
-  },[])
-
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data);
+      })
+  }, [])
   console.log(products);
   return (
     <>
@@ -21,10 +19,21 @@ function App() {
       <div className="main-container">
         <div className="cards-container">
           {
-            products.map((pd)=>(
-            <SingleProduct product={pd}></SingleProduct>))
+            products.map(pd=> <SingleProduct></SingleProduct>)
           }
-          
+          <div className="card">
+            <img className="card-img" src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt="" />
+            <h1>Tesr Test</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo laboriosam quam eaque aliquam mollitia ab.</p>
+            <div className="card-footer">
+              <h1>520 $</h1>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src="" alt="" />
+          </div>
         </div>
         <div className="cart-container">
           <h1>This is cart</h1>
