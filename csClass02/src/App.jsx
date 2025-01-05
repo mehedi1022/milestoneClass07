74823b6dd9ffacb478b8eda6b5c61a1d066bf131
mehedi1022 <mahdibinalamgir@gmail.com>
2025-01-05 21:43:44 +0600

    added setCart([p]) in app.jsx file

--- csClass02/src/App.jsx
+++ csClass02/src/App.jsx
@@ -5,6 +5,8 @@ import SingleProduct from "./singleProduct";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
   useEffect(() => {
     fetch("./fakeData.json")
       .then(res => res.json())
@@ -12,20 +14,30 @@ function App() {
         setProducts(data);
       })
   }, [])
-  console.log(products);
+
+  const handleCart = (p) => {
+    console.log(p);
+    setCart([p]);
+  }
+  console.log(cart);
+
   return (
     <>
 
       <div className="main-container">
         <div className="cards-container">
           {
-            products.map(pd=> <SingleProduct product={pd}></SingleProduct>)
+            products.map(pd => <SingleProduct product={pd} handleCart={handleCart}></SingleProduct>)
           }
-          
+
 
         </div>
         <div className="cart-container">
           <h1>This is cart</h1>
+          <div className="cart-title">
+            <h5>Name</h5>
+            <h5>Price</h5>
+          </div>
         </div>
       </div>
 
--- csClass02/src/SingleProduct.jsx
+++ csClass02/src/SingleProduct.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-const SingleProduct = ({product}) => {
-    console.log(product);
-    return (
-        <div>
-            <div className="card">
-            <img className="card-img" src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt="" />
-            <h1>Tesr Test</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo laboriosam quam eaque aliquam mollitia ab.</p>
-            <div className="card-footer">
-              <h1>520 $</h1>
-              <button>Add To Cart</button>
-            </div>
-          </div>
+const SingleProduct = ({ product, handleCart }) => {
+  // console.log(handleCart);
+  return (
+    <div>
+      <div className="card">
+        <img className="card-img" src={product.image} alt="" />
+        <h1>{product.title.slice(0, 10)}</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo laboriosam quam eaque aliquam mollitia ab.</p>
+        <div className="card-footer">
+          <h1>{product.price} $</h1>
+          <button className="add-btn" onClick={(e) => handleCart(product)}>Add To Cart</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SingleProduct;
